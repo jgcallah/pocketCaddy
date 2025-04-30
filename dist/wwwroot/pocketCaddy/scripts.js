3c9d97dee18be60a467a9160de63a53e7bfa6959
jgcallah <justin106698@gmail.com>
2025-04-30 16:03:52 +0000

    Deploying to gh-pages from @ jgcallah/pocketCaddy@5c6a0b4c8e14650c686d88586bfdf8b5749e9c41 🚀

--- dist/wwwroot/pocketCaddy/scripts.js
+++
@@ -1,9 +0,0 @@
-window.getElementRect = (element) => {
-  const rect = element.getBoundingClientRect();
-  return {
-    left: rect.left,
-    top: rect.top,
-    width: rect.width,
-    height: rect.height,
-  };
-};
