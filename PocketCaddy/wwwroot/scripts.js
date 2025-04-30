d0caf44f30ea564011bb47210b3dad5890443a23
Justin Callahan <jcallahan@erikssonsoftware.com>
2025-04-30 09:19:47 -0400

    Initial push
    First push of new project

---
+++ PocketCaddy/wwwroot/scripts.js
@@ -0,0 +1,9 @@
+window.getElementRect = (element) => {
+  const rect = element.getBoundingClientRect();
+  return {
+    left: rect.left,
+    top: rect.top,
+    width: rect.width,
+    height: rect.height,
+  };
+};
