dde3a1621eaf3567059d46ddd04e0b3c512d6635
Justin Callahan <jcallahan@erikssonsoftware.com>
2025-05-02 11:16:33 -0400

    Implements compass and club data
    Adds a compass component with touch and mouse rotation.
    
    Adds club data for Blast, Mammoth, Everest, and Pulse clubs.
    
    Updates the Home page to use the new compass component and club data.

--- PocketCaddy/Components/ClubData.cs
+++ PocketCaddy/Components/ClubData.cs
@@ -2,15 +2,170 @@ public class ClubData
 {
     public string Name = string.Empty;
     public double MagicNumber;
+    public double Accuracy;
     public int Type;
     public double MaxRange;
     public double ShotView;
 
-    public static ClubData Eclipse(int level)
+    public static ClubData Blast_7()
     {
         var rtn = new ClubData()
         {
-            Name = $"Eclipse, {level}",
+            Name = $"Blast, 7*",
+            MagicNumber = 0.4926,
+            Type = 0,
+            Accuracy = 41,
+            MaxRange = 240 * 1.1,
+            ShotView = 0.93
+        };
+        return rtn;
+    }
+    
+    public static ClubData Mammoth_7()
+    {
+        var rtn = new ClubData()
+        {
+            Name = $"Mammoth, 7*",
+            MagicNumber = 0.8368,
+            Type = 0,
+            Accuracy = 75,
+            MaxRange = 235 * 1.1,
+            ShotView = 0.96
+        };
+        return rtn;
+    }
+    
+    public static ClubData Mammoth_8()
+    {
+        var rtn = new ClubData()
+        {
+            Name = $"Mammoth, 8",
+            MagicNumber = 0.9569,
+            Type = 0,
+            Accuracy = 80,
+            MaxRange = 238 * 1.1,
+            ShotView = 1
+        };
+        return rtn;
+    }
+    
+    public static ClubData Mammoth_9()
+    {
+        var rtn = new ClubData()
+        {
+            Name = $"Mammoth, 9*",
+            MagicNumber = 0.9901,
+            Type = 0,
+            Accuracy = 80,
+            MaxRange = 240 * 1.1,
+            ShotView = 1
+        };
+        return rtn;
+    }
+    
+    public static ClubData Everest_6()
+    {
+        var rtn = new ClubData()
+        {
+            Name = $"Everest, 6",
+            MagicNumber = 0.8065,
+            Type = 0,
+            Accuracy = 70,
+            MaxRange = 245 * 1.1,
+            ShotView = 0.85
+        };
+        return rtn;
+    }
+    
+    public static ClubData Everest_7()
+    {
+        var rtn = new ClubData()
+        {
+            Name = $"Everest, 7*",
+            MagicNumber = 0.8696,
+            Type = 0,
+            Accuracy = 75,
+            MaxRange = 245 * 1.1,
+            ShotView = 0.90
+        };
+        return rtn;
+    }
+    
+    public static ClubData Everest_8()
+    {
+        var rtn = new ClubData()
+        {
+            Name = $"Everest, 8*",
+            MagicNumber = 0.9009,
+            Type = 0,
+            Accuracy = 75,
+            MaxRange = 247 * 1.1,
+            ShotView = 0.98
+        };
+        return rtn;
+    }
+    
+    public static ClubData Pulse_6()
+    {
+        var rtn = new ClubData()
+        {
+            Name = $"Pulse, 6",
+            MagicNumber = 0.6897,
+            Type = 0,
+            Accuracy = 62,
+            MaxRange = 253 * 1.1,
+            ShotView = 0.7
+        };
+        return rtn;
+    }
+    
+    public static ClubData Pulse_7()
+    {
+        var rtn = new ClubData()
+        {
+            Name = $"Pulse, 7*",
+            MagicNumber = 0.813,
+            Type = 0,
+            Accuracy = 70,
+            MaxRange = 255 * 1.1,
+            ShotView = 0.8
+        };
+        return rtn;
+    }
+    
+    public static ClubData Pulse_8()
+    {
+        var rtn = new ClubData()
+        {
+            Name = $"Pulse, 8*",
+            MagicNumber = 0.8475,
+            Type = 0,
+            Accuracy = 70,
+            MaxRange = 255 * 1.1,
+            ShotView = 0.91
+        };
+        return rtn;
+    }
+    
+    public static ClubData CLUBNAME_LVL()
+    {
+        var rtn = new ClubData()
+        {
+            Name = $"CLUNNAME, LVL",
+            MagicNumber = 0.0,
+            Type = 0,
+            Accuracy = 0,
+            MaxRange = 0 * 1.1,
+            ShotView = 0
+        };
+        return rtn;
+    }
+
+    public static ClubData Eclipse_10()
+    {
+        var rtn = new ClubData()
+        {
+            Name = $"Eclipse, 10",
             MagicNumber = 0.925,
             Type = 2,
             MaxRange = 185,
