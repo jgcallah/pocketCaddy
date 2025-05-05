23cf2a4bddf0f9eb87e19e25e6471f1489ff16ab
Justin Callahan <jcallahan@erikssonsoftware.com>
2025-05-05 08:54:50 -0400

    Adds rollout options to settings
    Introduces rollout options to the settings model.
    
    The prior state of the settings model had conflicting
    definitions due to unresolved merge conflicts. This commit
    resolves those conflicts and adds the rollout options.

--- PocketCaddy/Model/Settings.cs
+++ PocketCaddy/Model/Settings.cs
@@ -1,6 +1,16 @@
+<<<<<<< Updated upstream
 public class Settings
 {
     public PullOptions Pull;
     public double ZeroWedgeRange;
     public double 
+=======
+namespace PocketCaddy.Model
+{
+    public class Settings
+    {
+        public PullOptions Pull;
+        public RolloutOptions Rollout;
+    }
+>>>>>>> Stashed changes
 }
